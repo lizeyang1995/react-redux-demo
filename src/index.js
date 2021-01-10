1de45586bfb12d2683efe22bc369ef94cdcfe132
lizeyang1995 <424011527@qq.com>
2021-01-10 14:57:12 +0800

    [lizeyang] feat: add react redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import TodoList from './TodoList';
+import { Provider } from 'react-redux'
+import store from './store/index'
+//被Provider包裹的标签里都可以用store里的值
+const App = (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  App,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+    inputValue: 'lzy',
+    list: []
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = defaultState, action) => {
+    return state
+}
