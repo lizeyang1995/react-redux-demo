2249d87bd6476074e6c3df5a24f4ded49821ad51
lizeyang1995 <424011527@qq.com>
2021-01-10 15:08:13 +0800

    [lizeyang] feat: add provider and connect

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import store from './store/index'
+import { connect } from 'react-redux'
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = store.getState()
-    }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
                 <div>
-                    <input value={this.state.inputValue} />
+                    <input value={this.props.inputValue} />
                     <button>提交</button>
                 </div>
                 <ul>
                     <li>哈哈</li>
                 </ul>
             </div>
-         );
+        );
     }
 }
- 
-export default TodoList;
+
+//把state变成props
+const stateToProps = (state) => {
+    return {
+        inputValue: state.inputValue
+    }
+}
+
+//两个括号，表示函数的返回值还是一个函数
+//第一个参数是映射store的数据到props，第二个是触发dispatch，第二个参数可以不传
+export default connect(stateToProps, null)(TodoList);
