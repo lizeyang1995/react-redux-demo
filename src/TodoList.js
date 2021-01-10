9b7193a1674afb11ab961c3ade5cbb83efbb0f76
lizeyang1995 <424011527@qq.com>
2021-01-10 15:25:40 +0800

    [lizeyang] feat: add dispatchToProps

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,11 +3,12 @@ import store from './store/index'
 import { connect } from 'react-redux'
 
 class TodoList extends Component {
+
     render() {
         return (
             <div>
                 <div>
-                    <input value={this.props.inputValue} />
+                    <input value={this.props.inputValue} onChange={this.props.inputChange} />
                     <button>提交</button>
                 </div>
                 <ul>
@@ -25,6 +26,18 @@ const stateToProps = (state) => {
     }
 }
 
+const dispatchToProps = (dispatch) => {
+    return {
+        inputChange(e) {
+            let action = {
+                type: 'change_input',
+                value: e.target.value
+            }
+            dispatch(action)
+        }
+    }
+}
+
 //两个括号，表示函数的返回值还是一个函数
 //第一个参数是映射store的数据到props，第二个是触发dispatch，第二个参数可以不传
-export default connect(stateToProps, null)(TodoList);
+export default connect(stateToProps, dispatchToProps)(TodoList);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,5 +5,10 @@ const defaultState = {
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = defaultState, action) => {
+    if (action.type === "change_input") {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+        return newState
+    }
     return state
 }
